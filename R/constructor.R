b3de26cce5297bd5b27a633b0ffec3af5148ea3e
ksusvita92 <ksusvita@gmail.com>
2022-10-05 10:25:06 -0700

    generic functions

---
+++ R/constructor.R
@@ -0,0 +1,33 @@
+#' Define class "siestim"
+#'
+#' @param x
+#'
+#' @return
+#'
+new_siestim <- function(x){
+  structure(x, class = "siestim")
+}
+
+
+
+#' Define class plotsi
+#'
+#' @param x
+#'
+#' @return
+#'
+new_plotsi <- function(x){
+  structure(x, class = "plotsi")
+}
+
+
+
+#' Define class simdata
+#'
+#' @param x
+#'
+#' @return
+#'
+new_simdata <- function(x){
+  structure(x, class = "simdata")
+}
