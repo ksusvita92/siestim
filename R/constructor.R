9d7646ac01e709982b46e8be3179ac84e9d7afd6
Kurnia Susvitasari <ksusvita@sfu.ca>
2022-12-16 13:33:34 -0800

    Generic functions

--- R/constructor.R
+++ R/constructor.R
@@ -1,33 +1,12 @@
 #' Define class "siestim"
-#'
-#' @param x
-#'
-#' @return
-#'
-new_siestim <- function(x){
-  structure(x, class = "siestim")
-}
+new_siestim <- function(x) structure(x, class = "siestim")
 
 
 
 #' Define class plotsi
-#'
-#' @param x
-#'
-#' @return
-#'
-new_plotsi <- function(x){
-  structure(x, class = "plotsi")
-}
+new_plotsi <- function(x) structure(x, class = "plotsi")
 
 
 
-#' Define class simdata
-#'
-#' @param x
-#'
-#' @return
-#'
-new_simdata <- function(x){
-  structure(x, class = "simdata")
-}
+#' Create generic function getci
+getci <- function(x, level = .95) UseMethod("getci", x)
