28a38abdf799ffb131d2f11f3838f28dc7b6648b
Kurnia Susvitasari <ksusvita@sfu.ca>
2022-12-16 22:24:41 -0800

    Utility functions

--- R/utils.R
+++ R/utils.R
@@ -15,11 +15,17 @@ logll <- function(params, dt, ctr = list()){
 
   f1 <- dcgg(dt, params[1], params[2], params[3])
   f2 <- dfgd(dt, params[1], params[2])
+  #avoid log(0), log(Inf); give some offsets
+  f1[f1==0 | !is.finite(f1)] <- f2[f2==0 | !is.finite(f1)] <- 1e-300 #non-finite occurs when e.g dgamma(0,.1,2)
   ft <- params[4]*f1 + (1-params[4])*f2
+  if(fm == 0 || !is.finite(fm)) fm <- 1e-300
+  if(fs == 0 || !is.finite(fs)) fs <- 1e-300
+  if(fp == 0 || !is.finite(fp)) fp <- 1e-300
+  if(fw == 0 || !is.finite(fw)) fw <- 1e-300
+
 
   # result
   nll <- -sum(log(ft))-log(fp)-log(fw)-log(fm)-log(fs)
-  if(!is.finite(nll)) nll <- log(1e-300) #dfoptim got error if nll if not finite.
 
   return(nll)
 }
@@ -42,10 +48,6 @@ opt <- function(params0, dt, lower, upper, ctr = list()){
   # omit NA
   dt <- dt[!is.na(dt)]
 
-  # check appropriate lb, ub, and init values
-  if(any(lower<0)) stop("Error: lower bound cannot be negative.")
-  if(any(upper[3:4]>1)) stop("Error: parameter pi & w must not exceed 1.")
-  if(any(params0<=lower) || any(params0>=upper)) stop("Error: initial values must be within lower & upper bounds.")
 
   # estimate parameters
   myestim <- dfoptim::nmkb(params0, logll, lower, upper, dt=dt)
