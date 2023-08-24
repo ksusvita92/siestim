783e04cad37006facca265b9b21607a07b5510e3
Kurnia Susvitasari <ksusvita@sfu.ca>
2023-08-24 13:17:45 -0700

    Generic functions

--- R/constructor.R
+++ R/constructor.R
@@ -8,9 +8,16 @@ new_plotsi <- function(x) structure(x, class = "plotsi")
 
 
 
+#' Define class outbreak
+new_simOutbreak <- function(x) structure(x, class = "simOutbreak")
+
+
 #' Create generic function getci
 #' @export
+getci <- function(x, level = .95) UseMethod("getci", x)
 getci2 <- function(x, level = .95) UseMethod("getci2", x)
 
-#'@export
-getci <- function(x, level = .95) UseMethod("getci", x)
+
+#' Create generic function createTC
+#' @export
+createTC <- function(x, downsampling_prob, cutoff, dna_model = "N", seed = 1) UseMethod("createTC", x)
